099de94416c57a06a9852adae6c5fc7d4d351f0f
Folk.pf <Tanawut.pf@gmail.com>
2025-09-03 23:16:14 +0700

    ServePage

--- app.ts
+++ app.ts
@@ -1,17 +1,27 @@
-import express from 'express';
-import chalk from 'chalk';
-import debug from 'debug';
-import morgan from 'morgan';
+import express from "express";
+import morgan from "morgan";
+import chalk from "chalk";
+import createDebug from "debug";
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
+import "dotenv/config";
+
+const __filename = fileURLToPath(import.meta.url); // file:///D:/test/app.ts
+const __dirname = dirname(__filename); // => D:\test\app.ts   (บน Windows)
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = Number(process.env.PORT) || 3000;
 
-app.use(morgan('combined'));
+app.use(morgan("dev"));
 
-app.get('/', (req, res) => {
-  res.send('Hello world');
+app.use(express.static(join(__dirname, "public")));
+
+app.get("/", (_req, res) => {
+  res.send("Hello From app.ts");
 });
 
+const debug = createDebug("app");
 app.listen(port, () => {
-  debug(`Listening on port ${chalk.green(": "+port)}`);
+  debug(`Listening on ${chalk.green(String(port))}`);
+  console.log(`Listening on ${chalk.green(String(port))}`);
 });
